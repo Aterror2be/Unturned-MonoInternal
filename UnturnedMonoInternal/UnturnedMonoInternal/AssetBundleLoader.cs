cd9e3317c2d95aef159b1b1a9f8d77b9c1f485f5
Aterror2be <rjnobriga@gmail.com>
2021-08-18 18:11:41 -0500

    Testing fix

--- UnturnedMonoInternal/UnturnedMonoInternal/AssetBundleLoader.cs
+++ UnturnedMonoInternal/UnturnedMonoInternal/AssetBundleLoader.cs
@@ -5,14 +5,14 @@ class AssetBundleLoader : MonoBehaviour
 {
     public static AssetBundle assetBundle;
     public static Shader chamsShader;
-    //public static GUISkin skin = null;
+    public static GUISkin skin = null;
     string path = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
     string folder = "/AssetBundles/cheat";
 
     public void Start()
     {
         assetBundle = AssetBundle.LoadFromFile(path + folder);
-        //skin = assetBundle.LoadAsset<GUISkin>("MenuSkin");
+        skin = assetBundle.LoadAsset<GUISkin>("MenuSkin");
         chamsShader = assetBundle.LoadAsset<Shader>("WireFrameChams");
     }
 }
